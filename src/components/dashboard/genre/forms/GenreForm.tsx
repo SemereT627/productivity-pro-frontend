a223f9de08f643f417daee18411d2012555bc193
Semere Talegngeta <se.semere.talegngeta@gmail.com>
2024-02-05 13:58:01 +0300

    feat: genre create, update and read

---
+++ src/components/dashboard/genre/forms/GenreForm.tsx
@@ -0,0 +1,171 @@
+import { Button, Col, Form, Input, Row } from "antd";
+import { Genre } from "../../../../store/types/genre.types";
+import { useDispatch } from "react-redux";
+import { RootStates } from "../../../../store/interface";
+import { useSelector } from "react-redux";
+import {
+  clearCreateGenre,
+  clearUpdateGenre,
+  createGenreStart,
+  updateGenreStart,
+} from "../../../../store/features/genre.slice";
+import { useEffect } from "react";
+import { globalNotification } from "../../../../utils/notifications";
+import styled from "@emotion/styled";
+
+type GenreFormProps = {
+  isEdit?: boolean;
+  genre?: Genre;
+  onClose: () => void;
+};
+
+const GenreForm = ({ isEdit, genre, onClose }: GenreFormProps) => {
+  /**
+   * states
+   */
+
+  /**
+   * hooks
+   */
+  const dispatch = useDispatch();
+  const [form] = Form.useForm();
+
+  /**
+   * selectors
+   */
+  const { createGenreSuccess, loading, error, updateGenreSuccess } =
+    useSelector((state: RootStates) => state.genres);
+
+  /**
+   * functions
+   */
+  const handleSubmit = (values: Genre) => {
+    if (isEdit) dispatch(updateGenreStart({ ...values, _id: genre?._id }));
+    else dispatch(createGenreStart(values));
+  };
+
+  const onReset = () => {
+    form.resetFields();
+  };
+
+  /**
+   * effects
+   */
+
+  useEffect(() => {
+    if (createGenreSuccess) {
+      globalNotification("success", "Genre created successfully");
+      onClose();
+      form.resetFields();
+      dispatch(clearCreateGenre());
+    }
+
+    if (updateGenreSuccess) {
+      globalNotification("info", "Genre updated successfully");
+      onClose();
+      form.resetFields();
+      dispatch(clearUpdateGenre());
+    }
+
+    if (error) {
+      globalNotification("error", error);
+      dispatch(clearCreateGenre());
+      dispatch(clearUpdateGenre());
+    }
+  }, [createGenreSuccess, onClose, form, dispatch, updateGenreSuccess, error]);
+
+  /**
+   * yup and formik
+   */
+
+  /**
+   * variables
+   */
+
+  const FormItemStyled = styled(Form.Item)`
+    margin-bottom: 8px;
+  `;
+
+  return (
+    <Form
+      form={form}
+      name="genreForm"
+      layout="vertical"
+      onFinish={handleSubmit}
+      validateMessages={{ required: "This field is required" }}
+      scrollToFirstError
+      requiredMark={false}
+      style={{ height: "100%" }}
+      initialValues={
+        isEdit
+          ? {
+              name: genre?.name,
+            }
+          : {}
+      }
+    >
+      <Row
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+        gutter={16}
+      >
+        <Col span={24}>
+          <Row>
+            <Col span={24}>
+              <FormItemStyled
+                label="Name"
+                name="name"
+                rules={[{ required: true }]}
+              >
+                <Input placeholder="Type the genre name" />
+              </FormItemStyled>
+            </Col>
+          </Row>
+        </Col>
+        <Col
+          style={{
+            width: "100%",
+            marginTop: "auto",
+          }}
+        >
+          <Row gutter={16}>
+            {!isEdit && (
+              <Col span={12}>
+                <FormItemStyled style={{ width: "100%" }}>
+                  <Button
+                    onClick={onReset}
+                    style={{ width: "100%" }}
+                    type="dashed"
+                    danger
+                  >
+                    Clear
+                  </Button>
+                </FormItemStyled>
+              </Col>
+            )}
+            <Col span={isEdit ? 24 : 12}>
+              <FormItemStyled style={{ width: "100%" }}>
+                <Button
+                  htmlType="submit"
+                  style={{ width: "100%" }}
+                  type="primary"
+                  loading={loading}
+                  disabled={loading}
+                >
+                  {isEdit ? "Update" : "Submit"}
+                </Button>
+              </FormItemStyled>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default GenreForm;
--- src/pages/dashboard/DashboardGenrePage.tsx
+++ src/pages/dashboard/DashboardGenrePage.tsx
@@ -1,15 +1,22 @@
 import styled from "@emotion/styled";
-import { Button, Table, Typography } from "antd";
+import { Button, Popconfirm, Table, Typography } from "antd";
 import { useDispatch } from "react-redux";
 import { RootStates } from "../../store/interface";
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchGenresStart } from "../../store/features/genre.slice";
+import { Genre } from "../../store/types/genre.types";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import CustomDrawer from "../../components/common/Drawer";
+import GenreForm from "../../components/dashboard/genre/forms/GenreForm";
 
 const DashboardGenrePage = () => {
   /**
    * states
    */
+  const [openCreateGenreDrawer, setOpenCreateGenreDrawer] = useState(false);
+  const [openEditGenreDrawer, setOpenEditGenreDrawer] = useState(false);
+  const [selectedGenre, setSelectedGenre] = useState<Genre>();
 
   /**
    * hooks
@@ -24,6 +31,13 @@ const DashboardGenrePage = () => {
   /**
    * functions
    */
+  const handleCreateGenreDrawerClose = () => {
+    setOpenCreateGenreDrawer(false);
+  };
+
+  const handleEditGenreDrawerClose = () => {
+    setOpenEditGenreDrawer(false);
+  };
 
   /**
    * effects
@@ -48,6 +62,32 @@ const DashboardGenrePage = () => {
     {
       title: "Actions",
       key: "actions",
+      render: (genre: Genre) => (
+        <>
+          <Button
+            type="link"
+            icon={<EditOutlined />}
+            onClick={() => {
+              setSelectedGenre(genre);
+              setOpenEditGenreDrawer(true);
+            }}
+          >
+            Edit
+          </Button>
+          <Popconfirm
+            title="Are you sure?"
+            okText="Yes"
+            okType="danger"
+            cancelText="No"
+            // onConfirm={() => dispatch(deleteDriverAsync(id))}
+            // disabled={deleteDriverLoading}
+          >
+            <Button danger>
+              <DeleteOutlined /> Delete
+            </Button>
+          </Popconfirm>
+        </>
+      ),
     },
   ];
 
@@ -64,7 +104,14 @@ const DashboardGenrePage = () => {
         <Typography.Title level={4} style={{ margin: 0 }}>
           Genres
         </Typography.Title>
-        <Button type="primary">Create Genre</Button>
+        <Button
+          type="primary"
+          onClick={() => {
+            setOpenCreateGenreDrawer(true);
+          }}
+        >
+          Create Genre
+        </Button>
       </TableHeaderWrapper>
 
       <Table
@@ -73,6 +120,28 @@ const DashboardGenrePage = () => {
         columns={columns}
         dataSource={genres}
       />
+
+      <CustomDrawer
+        title={"Create Genre"}
+        onClose={handleCreateGenreDrawerClose}
+        open={openCreateGenreDrawer}
+      >
+        <GenreForm
+          genre={selectedGenre}
+          onClose={handleCreateGenreDrawerClose}
+        />
+      </CustomDrawer>
+      <CustomDrawer
+        title={"Edit Genre"}
+        onClose={handleEditGenreDrawerClose}
+        open={openEditGenreDrawer}
+      >
+        <GenreForm
+          isEdit
+          genre={selectedGenre}
+          onClose={handleEditGenreDrawerClose}
+        />
+      </CustomDrawer>
     </>
   );
 };
